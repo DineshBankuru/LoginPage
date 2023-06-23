7fad2c1e15d3aecce0d43fd40989a77620818e45
DineshBankuru <108983416+DineshBankuru@users.noreply.github.com>
2023-06-23 17:40:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -362,8 +362,14 @@ app.post("/login" , function(req,res){
 
 
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, function() {
+    console.log("Server started!!");
 });
+  
+
   
 
